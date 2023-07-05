e190ce71f2b62f7f028fad56b0dcc4b738ab5b39
horsepork <kennydhubbell@gmail.com>
2023-07-05 11:27:36 -0500

    Initial commit
    Library file (header only) and example ino

---
+++ Keypad_MCP23008.h
@@ -0,0 +1,267 @@
+#include <Adafruit_MCP23X08.h>
+
+class Keypad_MCP23008{
+    public:
+        Keypad_MCP23008(uint8_t _numRows, uint8_t _numCols, const uint8_t *_rowPins, const uint8_t *_colPins, uint8_t _address, TwoWire *_wire){
+            numRows = _numRows;
+            numCols = _numCols;
+            rowPins = _rowPins;
+            colPins = _colPins;
+            address = _address;
+            wire = _wire;
+            mcp = new Adafruit_MCP23X08(); // necessary?
+        }
+    private:
+        uint8_t address;
+        uint8_t numRows;
+        uint8_t numCols;
+        const uint8_t *rowPins;
+        const uint8_t *colPins;
+        TwoWire* wire;
+        Adafruit_MCP23X08* mcp;
+        uint8_t readDelay = 10;
+        uint8_t debounceTime = 1;
+        uint32_t timer;
+        uint8_t GPIO_State = 0;
+        bool resetNeeded = false;
+        uint16_t prevState = 0;
+        uint16_t outputState = 0;
+        uint16_t debouncedBooleanOutputState = 0;
+        uint8_t debouncedOutputState = 0;
+        const uint16_t errorVal = 65535; // max val of uint16_t, "-1"
+        bool debug = false;
+        bool updated = false;
+
+    
+    public:
+        void begin(){
+            wire->setClock(400000);
+            mcp->begin_I2C(address, wire);
+            for(int i = 0; i < numRows; i++){
+                mcp->pinMode(i, INPUT_PULLUP);
+            }
+            for(int i = numRows; i < numRows + numCols; i++){
+                mcp->pinMode(i, OUTPUT);
+                mcp->digitalWrite(i, HIGH);
+            }
+            GPIO_State = mcp->readGPIO();
+            timer = millis();
+        }
+
+        void setReadDelay(uint8_t d){
+            readDelay = d;
+            if(!debug) return;
+            Serial.print("read delay -- ");
+            Serial.println(readDelay);
+        }
+
+        void setDebounceTime(uint8_t d){
+            debounceTime = d;
+            if(!debug) return;
+            Serial.print("debounce time -- ");
+            Serial.println(debounceTime);
+        }
+
+        void update(){
+            if(millis() - timer < readDelay){
+                return;
+            }
+            updateDebouncedOutputState(outputState);
+            timer = millis();
+            if(resetNeeded){
+                if(!keypadReset()){
+                    return;
+                }
+            }
+            unsigned int newState = getNewState();
+            int continuousBits = 0;
+            if(newState == 0){
+                outputState = 0;
+                prevState = 0;
+                return;
+            }
+            for(int i = 0; i < numRows * numCols; i++){
+                if(bitRead(newState, i)){
+                    if(++continuousBits > numCols){
+                        resetNeeded = true;
+                        outputState = errorVal;
+                        return;
+                    }
+                }
+                else{
+                    continuousBits = 0;
+                }
+            }
+            if(prevState != newState){
+                unsigned int newBits = ~prevState & newState;
+                int numNewBits = numBits(newBits);
+                if(numNewBits == 1){
+                    outputState = newBits;
+                }
+                else if(numNewBits == 0){
+                    outputState = 0;
+                }
+                prevState = newState;
+            }
+        }
+
+        uint8_t read(bool print = false){
+            if(print && debug){
+                Serial.print("Keypad output --");
+                Serial.println(debouncedOutputState);
+            }
+            updated = false;
+            return debouncedOutputState;
+        }
+
+        uint16_t readBoolean(bool print = false){
+            if(print && debug){
+                Serial.print("Keypad output, boolean mode -- ");
+                printBinary(debouncedBooleanOutputState, numRows * numCols);
+            }
+            updated = false;
+            return debouncedBooleanOutputState;
+        }
+
+        void printBinary(int num, int digits = 8){
+            if(!debug) return;
+            if(num == 0){
+                for(int i = 0; i < digits; i++){
+                    Serial.print('0');
+                }
+                Serial.println();
+                return;
+            }
+            int _num = num;
+            int counter = 0;
+            while(_num > 0){
+                _num >>= 1;
+                counter++;
+            }
+            if(counter < digits){
+                for(int i = 0; i < digits - counter; i++){
+                    Serial.print('0');
+                }
+            }
+            Serial.println(num, BIN);
+        }
+
+        void setDebugMode(bool d){
+            debug = d;
+            Serial.print("Keypad debugging ");
+            debug ? Serial.println("on") : Serial.println("off");
+        }
+
+        bool isConnected() {
+            wire->beginTransmission(address);
+            return wire->endTransmission() == 0;
+        }
+
+        bool checkIfUpdated(){
+            return updated;
+        }
+
+
+        
+    private:
+        void switchCol(int col){
+            for(int i = 0; i < numCols; i++){
+                if(i == col){
+                    bitClear(GPIO_State, colPins[i]);
+                }else{
+                    bitSet(GPIO_State, colPins[i]);
+                }
+            }
+            mcp->writeGPIO(GPIO_State);
+        }
+
+        int power(int base, int exp){
+            int val = 1;
+            for(int i = 0; i < exp; i++){
+                val *= base;
+            }
+            return val;
+        }
+
+        bool keypadReset(){
+            if(isConnected()){ // why? possible endless loop?
+                // return false;
+            }
+            writeReg(0x06, 0xFF);
+            for(int i = 0; i < numCols; i++){
+                mcp->pinMode(i, INPUT_PULLUP);
+            }
+            for(int i = numRows; i < numRows + numCols; i++){
+                mcp->pinMode(i, OUTPUT);
+                mcp->digitalWrite(i, HIGH);
+            }
+            resetNeeded = false;
+            if(debug){
+                Serial.print("Keypad successfully reset");
+            }
+            return true;
+        }
+
+        bool writeReg(uint8_t reg, uint8_t value){
+            wire->beginTransmission(address);
+            wire->write(reg);
+            wire->write(value);
+            return wire->endTransmission() == 0;
+        }
+
+        int numBits(int num){
+            if(num < 0){
+                return 0;
+            }
+            int counter = 0;
+            while(num > 0){
+                counter += bitRead(num, 0);
+                num >>= 1;
+            }
+            return counter;
+        }
+
+        int getNewState(){
+            int _newState = 0;
+            for(int c = 0; c < numCols; c++){
+                switchCol(c);
+                GPIO_State = mcp->readGPIO();
+                for(int r = 0; r < numRows; r++){
+                    if(!bitRead(GPIO_State, rowPins[r])){
+                        uint8_t button = c + 1 + numCols * r;
+                        _newState += power(2, button - 1);
+                    }
+                }
+            }
+            return _newState;
+        }
+
+        void updateDebouncedOutputState(int val){
+            static int prevVal = 0;
+            static int debounceCounter = 0;
+            if(val == debouncedBooleanOutputState){
+                debounceCounter = 0;
+                return;
+            }
+            if(prevVal != val){
+                debounceCounter = 0;
+                prevVal = val;
+            }
+            else if(++debounceCounter > (val != 0 ? debounceTime : debounceTime * 3)){
+                debouncedBooleanOutputState = val;
+                if(debouncedBooleanOutputState == errorVal){
+                    debouncedOutputState = 255;
+                    updated = true;
+                    return;
+                }
+                debouncedOutputState = 0;
+                for(int i = 0; i < numRows * numCols; i++){
+                    if(bitRead(debouncedBooleanOutputState, i)){
+                        debouncedOutputState = i + 1;
+                        updated = true;
+                        break;
+                    }
+                }
+            }
+        }
+};
---
+++ keypad_example.ino
@@ -0,0 +1,43 @@
+#include "Arduino.h"
+#include "Keypad_MCP23008.h"
+
+#define I2C0_SDA_PIN 0 // change depending on which pin is used
+#define I2C0_SCL_PIN 1 // change depending on which pin is used
+#define MCP23008_I2C_ADDRESS 0x20 // default value if no jumpers, can range from 0x20 to 0x28
+
+#define NUM_ROWS 3 // can be up to 4 for a 16 button keypad
+#define NUM_COLS 4
+
+// may need to change depending on keypad pin layout
+const uint8_t* rowPins = {0, 1, 2};
+const uint8_t* colPins = {3, 4, 5, 6};
+
+// can use I2C0 or I2C1 (each have different pin options, check Pico pinout)
+// If I2C0, use Wire. If I2C1, use Wire1
+Keypad_MCP23008 keypad(NUM_ROWS, NUM_COLS, rowPins, colPins, MCP23008_I2C_ADDRESS, &Wire);
+
+void setup(){
+    Serial.begin(115200);
+    Wire.setSDA(I2C0_SDA_PIN);
+    Wire.setSCL(I2C0_SCL_PIN);
+    keypad.begin();
+    keypad.setUpdateDelay(20); // optional, change time between updates. Defaults to 10ms
+    keypad.setDebounceCounter(3); // optional. Increase if experiencing weird behavior. Defaults to 1
+    //keypad.setDebugMode(true); // turns on debugging, off by default, needs to be off when communicated with Unity
+}
+
+void loop(){
+    keypad.update(); // call every loop
+    if(keypad.checkIfUpdated()){
+        Serial.println(keypad.read()); // keypad.read() will always return the current state, returns values "normally"
+                                       // it will also reset the "updated" flag
+                                       // in binary, will return "1" as 00000001, "2" as 00000010, 3 as 00000011, etc.
+                                       // will return 255 if error. Should automatically reset if error
+
+        // keypad.printBinary(keypad.readBoolean(), NUM_ROWS * NUM_COLS); // printBinary only works if debugging is on
+        // readBoolean will return "1" as 000000000001, "2" as 000000000010, etc.
+        // will return 65535 if error
+        // readBoolean not recommended for use. Has to return an int instead of byte
+    }
+    
+}
